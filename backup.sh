f3e9c83b5daca078a3e200ad980be793d97eb6f6
Myles Braithwaite <myles@users.noreply.github.com>
2016-01-10 19:05:36 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,31 +11,28 @@ git pull origin master
 
 t timeline $TWITTER_USER --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/tweets.csv
 
-t retweets --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/retweets.csv
+t retweets $TWITTER_USER --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/retweets.csv
 
-t favorites --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/favorites.csv
+t favorites $TWITTER_USER --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/favorites.csv
 
 t direct_messages --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/dm_received.csv
 t direct_messages_sent --csv --decode-uris --number 3000 > ${ARCHIVEHOME}/dm_sent.csv
 
-t whoami > ${ARCHIVEHOME}/profile.txt
+t whois $TWITTER_USER > ${ARCHIVEHOME}/profile.txt
 
 t lists -l --csv > ${ARCHIVEHOME}/lists.csv
 
 for list in `t lists -i`
 do
-  if [ "$list" == "$TWITTER_USER"* ];
-  then
-    t list members $list -l --csv > ${ARCHIVEHOME}/lists/${list//${TWITTER_USER,,}\/}.csv
-  fi
+  t list members $list -l --csv > ${ARCHIVEHOME}/lists/${list//${TWITTER_USER,,}\/}.csv
 done
 
 gpg --encrypt --armor -r "${GPGKEYS}" --batch --yes --trust-model always -o ${ARCHIVEHOME}/dm_received.csv.gpg ${ARCHIVEHOME}/dm_received.csv
 gpg --encrypt --armor -r "${GPGKEYS}" --batch --yes --trust-model always -o ${ARCHIVEHOME}/dm_sent.csv.gpg ${ARCHIVEHOME}/dm_sent.csv
 gpg --encrypt --armor -r "${GPGKEYS}" --batch --yes --trust-model always -o ${ARCHIVEHOME}/lists/\@mylesb-awesome-people.csv.gpg  ${ARCHIVEHOME}/lists/\@mylesb-awesome-people.csv
 
-t followings --csv > ${ARCHIVEHOME}/followings.csv
-t followers --csv > ${ARCHIVEHOME}/followers.csv
+t followings $TWITTER_USER --csv > ${ARCHIVEHOME}/followings.csv
+t followers $TWITTER_USER --csv > ${ARCHIVEHOME}/followers.csv
 
 git add ${ARCHIVEHOME}
 git commit -m "script.sh automated checkin on `hostname -s`."
